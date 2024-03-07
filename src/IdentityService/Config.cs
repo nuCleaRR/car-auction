a346515a850f5625768b5c23b032385cbc5bbfd7
Oleksii Novitskyi <oleksii.novitskyi@cdw.com>
2024-03-07 14:52:19 -0800

    End of section 6

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -18,16 +18,28 @@ public static class Config
         };
 
     public static IEnumerable<Client> Clients =>
-        [
-            // interactive client using code flow + pkce
+        new Client[]
+        {
             new Client
             {
                 ClientId = "postman",
                 ClientName = "Postman",
                 AllowedScopes = {"openid", "profile", "auctionApp"},
-                RedirectUris ={"http://nothing"},
-                ClientSecrets = [new Secret("NotASecret".Sha256())],
+                RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
+                ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
                 AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
             },
-        ];
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
+                AllowOfflineAccess = true,
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                AccessTokenLifetime = 3600*24*30
+            }
+        };
 }
