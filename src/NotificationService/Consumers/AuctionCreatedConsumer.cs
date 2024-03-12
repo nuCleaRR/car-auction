af0cfdb475d8a0a19ca5de68c369ebbba8c5fab1
Oleksii Novitskyi <oleksii.novitskyi@cdw.com>
2024-03-11 21:23:17 -0700

    End of section 12

---
+++ src/NotificationService/Consumers/AuctionCreatedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+
+namespace NotificationService;
+
+public class AuctionCreatedConsumer : IConsumer<AuctionCreatedConsumer>
+{
+    private readonly IHubContext<NotificationHub> _hubContext;
+
+    public AuctionCreatedConsumer(IHubContext<NotificationHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task Consume(ConsumeContext<AuctionCreatedConsumer> context)
+    {
+        Console.WriteLine("---> auction created message received");
+
+        await _hubContext.Clients.All.SendAsync("AuctionCreated", context.Message);
+    }
+}
