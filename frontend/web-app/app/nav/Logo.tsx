d294baf37e402e11a29af533b88929bec7e538ce
Oleksii Novitskyi <oleksii.novitskyi@cdw.com>
2024-03-14 20:05:37 -0700

    End of section 14

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -18,7 +18,7 @@ export default function Logo() {
     return (
         <div onClick={doReset} className='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'>
             <AiOutlineCar size={34} />
-            <div>Carsties Auctions</div>
+            <div>Car Auctions</div>
         </div>
     )
 }
--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddMassTransit(x =>
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options => 
     {
-        options.Authority = builder.Configuration["IdentityServerUrl"];
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
         options.RequireHttpsMetadata = false;
         options.TokenValidationParameters.ValidateAudience = false;
         options.TokenValidationParameters.NameClaimType = "username";
